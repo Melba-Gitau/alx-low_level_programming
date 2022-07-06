c7297f11c161da68082da9941e80be47ab32d60e
Melba Gitau <melba.magiri@gmail.com>
2022-07-05 18:10:12 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,16 +15,16 @@ if (n > 0)
 	test = 1;
 	_putchar('+');
 }
-else if (n == 0)
+	else if (n == 0)
 {
 	test = 0;
 	_putchar('0');
 }
-else
+	else
 {
 	test = -1;
 	_putchar('-');
 }
 
-return (test);
+	return (test);
 }
