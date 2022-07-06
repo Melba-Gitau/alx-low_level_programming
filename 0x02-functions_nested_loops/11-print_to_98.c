22fe66689c2004285d2b1bb2349e4101860811ac
Melba Gitau <melba.magiri@gmail.com>
2022-07-06 11:55:17 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -38,4 +38,45 @@ while (n < 98)
 		_putchar((n / 10) + 48);
 		_putchar((n % 10) + 48);
 	}
-	elseif (n >= 0)
+	else if (n >= 0)
+	{
+		_putchar(n + 48);
+	}
+	else if (n > -10)
+	{
+		_putchar('-');
+		_putchar((n / 10) * -1 + 48);
+	}
+	else if (n > -100)
+	{
+		_putchar('-');
+		_putchar((n / 10) * -1 + 48);
+		_putchar((n % 10) * -1 + 48);
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(-1 * (n / 100) + 48);
+		_putchar(-1 * (n % 100) / 10 + 48);
+		_putchar(-1 * (n % 100) % 10 + 48);
+	}
+	_putchar(',');
+	_putchar('_');
+	n++;
+}
+}
+
+/**
+* print_to_98 - print any number to 98
+* @n: number to start from
+* Return: return result
+*/
+void print_to_98(int n)
+{
+	upperband(n);
+	lowerband(n);
+	_putchar((98 / 10) + 48);
+	_putchar((98 % 10) + 48);
+	putchar('\n');
+}
+
