71ed4d79439db3c2e7f49d0bd5dcfd874450cb05
Melba Gitau <melba.magiri@gmail.com>
2022-07-05 18:54:39 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ while (a <= 9)
 	}
 	else if (rep < 10)
 	{
-		_putchar('');
+		_putchar('_');
 		_putchar('0' + rep);
 	}
 	else
@@ -32,7 +32,7 @@ while (a <= 9)
 	if (b < 9)
 	{
 		_putchar(',');
-		_putchar('');
+		_putchar('_');
 	}
 	b++;
 }
