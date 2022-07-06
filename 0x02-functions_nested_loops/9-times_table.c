28f971370aeb1bbe60c1fe93bc735171c0cc6baa
Melba Gitau <melba.magiri@gmail.com>
2022-07-06 09:48:48 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,11 @@ int a = 0;
 int b;
 int rep;
 while (a <= 9)
-{
+	{
 	b = 0;
 	while(b <= 9)
- 		{	
+		
+	{	
 	rep = a * b;
 	if (b == 0)
 	{
@@ -23,7 +24,7 @@ while (a <= 9)
 	}
 	else if (rep < 10)
 	{
-		_putchar('_');
+		_putchar('');
 		_putchar('0' + rep);
 	}
 	else
@@ -35,7 +36,7 @@ while (a <= 9)
 	if (b < 9)
 	{
 		_putchar(',');
-		_putchar('_');
+		_putchar('');
 	}
 	b++;
 }
