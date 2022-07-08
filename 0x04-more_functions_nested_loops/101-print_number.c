d9818cb1fea91a5201732d063270c4bf92dab55b
Melba Gitau <melba.magiri@gmail.com>
2022-07-08 11:25:41 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,14 @@ unsigned int m;
 
 if (n < 0)
 {
-	_putchar('-');
-	m = -n;
+_putchar('-');
+m = -n;
 }
 else
 {
-	m = n;
+m = n;
 }
 if (m / 10 != 0)
-	print_number(m / 10);
-	_putchar((m % 10) + '0');
+print_number(m / 10);
+_putchar((m % 10) + '0');
 }
