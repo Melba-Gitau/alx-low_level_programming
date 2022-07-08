9dd9c9460313df631603d889490cd7562d005b80
Melba Gitau <melba.magiri@gmail.com>
2022-07-08 09:25:56 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
@@ -14,10 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-=======
-#ifndef MAIN_H
-#define MAIN_H
-int _isupper(int c);
->>>>>>> 81f03c5c2148ba8767121fb62c9bbb7239ab3310
 
 #endif
