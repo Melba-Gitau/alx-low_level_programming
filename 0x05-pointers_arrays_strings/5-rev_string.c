79080297b425a0ea49aa2330762d14f81c89c25e
Melba Gitau <melba.magiri@gmail.com>
2022-07-11 11:35:27 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* rev_string - prints a reverse string
+* @s: A pointer to an int that will be changed
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	char *start_C, *end_C, C;
+	int i, count;
+	int length = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		length++;
+	}
+	count = length;
+
+	start_C = S;
+	end_C = S;
+
+	for (i = 0; i < count - 1; i++)
+	{
+		end_C++;
+	}
+	for (i = 0; i < count / 2; i++)
+	{
+		C = *end_C;
+		*end_C = *start_C;
+		*start_C = C;
+		start_C++;
+		end_C--;
+	}
+}
+
