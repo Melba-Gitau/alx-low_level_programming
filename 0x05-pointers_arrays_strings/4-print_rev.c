3b78d0f0a47876838be2e81d0372e0f661b57fc7
Melba Gitau <melba.magiri@gmail.com>
2022-07-11 10:06:39 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1 ; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
