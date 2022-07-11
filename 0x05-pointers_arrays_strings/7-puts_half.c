299f4fe9e990c9db9f1e6d5d7289ef810194d89b
Melba Gitau <melba.magiri@gmail.com>
2022-07-11 13:23:10 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*puts_half - function that prints half a string
+*@str: this is input string
+*/
+void puts_half(char *str)
+{
+	int index, half;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+
+	half = index / 2
+		if (index % 2 == 1)
+			half++;
+
+	while (half < index)
+	{
+		_putchar(str[half]);
+	}
+	_putchar('\n');
+}
