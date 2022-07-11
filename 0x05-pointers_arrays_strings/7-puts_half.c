e8e07b3875fa9b4d642e9ff9084e15927f3cf9ac
Melba Gitau <melba.magiri@gmail.com>
2022-07-11 13:25:57 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 	while (str[index] != '\0')
 		index++;
 
-	half = index / 2
-		if (index % 2 == 1)
-			half++;
-
+	half = index / 2;
+	if (index % 2 == 1)
+		half++;
+	
 	while (half < index)
 	{
 		_putchar(str[half]);
