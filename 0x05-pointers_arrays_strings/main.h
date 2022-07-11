f9f0465126b28044b4a774ab504d302bb2b5b29c
Melba Gitau <melba.magiri@gmail.com>
2022-07-11 01:55:37 -0700

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,8 @@
 int _putchar(char c);
 void reset_to_98(int *m);
 void swap_int(int *a, int *b);
-int _strlen(char * s);
-void _puts(char * str);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
