a52bc670c62d41a23c56f41cfc8a649d938c0061
Melba Gitau <melba.magiri@gmail.com>
2022-07-13 23:29:46 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_number - prints an integer
- * @n: number to be printed
- * Return: Always 0 (Success)
+ * @n: integer to be printed
+ * 
  */
 void print_number(int n)
 {
