893c15fe81fbcd2dd9e86ef096c56c29874ea660
Melba Gitau <melba.magiri@gmail.com>
2022-07-13 23:16:34 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -9,7 +13,6 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * main - function that prints
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
