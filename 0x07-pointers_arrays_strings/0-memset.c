1989635ea3071862142efe617b5502bf00fc87a1
Melba Gitau <melba.magiri@gmail.com>
2022-07-18 11:33:31 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * char_memset - Fill memory with constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: byte of memory area to fill
+ * Return: the memory area to filled
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
