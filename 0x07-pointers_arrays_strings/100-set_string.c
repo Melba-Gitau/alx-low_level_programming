0e2d0db720549add60bf251aecb21dac4ab120e4
Melba Gitau <melba.magiri@gmail.com>
2022-07-18 13:54:29 -0700

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -10,3 +10,16 @@ void set_string(char **s, char *to)
 {
 	*s = to;
 }
+
+100-set_string.c
+#include "main.h"
+/**
+ * set_string - Sets the value of a pointer to a char
+ * @s: content to be copied
+ * @to: string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
