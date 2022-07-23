191ac21b06fc6e384937df2aedbeca4da06b4401
Melba Gitau <melba.magiri@gmail.com>
2022-07-23 13:21:31 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+* _strlen -> function to get length of string
+* @s: string pointer to be passed to these function
+* Return: returns length of the string
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - Copiar el contenido de uno a otra variable
+ * @dest: This is destiny
+ * @src: This is the copia
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
