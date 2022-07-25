774fe81ad06ad4b8b46ca2e0eafe28edf209893d
Melba Gitau <melba.magiri@gmail.com>
2022-07-25 10:45:29 -0700

    TASK 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,9 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0; j = 0; l = 0; k = 0;
+	int i = 0, j = 0, l = 0, k = 0;
+	char *s;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -35,9 +37,4 @@ char *str_concat(char *s1, char *s2)
 	}
 	s[k] = '\0';
 	return (s);
-}
-
-
-
-
-		
+}	
