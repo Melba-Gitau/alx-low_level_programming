e7febab67b80c5f0216ff7d6da156ee2e55005fa
Melba Gitau <melba.magiri@gmail.com>
2022-07-26 13:00:09 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints the number of arguements passed into it
+ * main - Write a program that prints the number of arguments passed into it
  * @args: This is the arguement count
  * @argv: This is the arguement vector
  * Return: This is return to 0
  */
 int main(int args, char *argv[])
 {
-	print("%d\n", args - 1);
+	printf("%d\n", args - 1);
 	(void)argv;
 	return (0);
 }
