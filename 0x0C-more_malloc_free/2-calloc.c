980cc6a10f5e84fcadd41d015f59ee559bc345ef
Melba Gitau <melba.magiri@gmail.com>
2022-07-28 09:59:36 -0700

    Task 0,1,2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * calloc -> calloc library internally implemented
+ * @nmemb: param 1
+ * @size: param 2
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	mem = malloc(size * nmemb);
+	if (mem = NULL)
+		return NULL;
+	filler = mem;
+	for (index = 0; index < (size * nmemb); index++)
+	{
+		filler[index] = '\0';
+	}
+	return (mem);
+}
